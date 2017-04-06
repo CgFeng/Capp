3bb97bede354f7fd1ac7a0e3f5fbc88c8d13fafa
urchankong@163.com <fcg0123>
2017-04-06 22:40:13 +0800

    增加图片加载的library

---
+++ imageloader/src/main/java/com/chengang/github/imageloader/ImageLoaderHelper.java
@@ -0,0 +1,14 @@
+package com.chengang.github.imageloader;
+
+/**
+ * Created by fengchengang on 2017/4/6.
+ * <p>
+ * 封装图片加载
+ */
+
+public class ImageLoaderHelper {
+
+    public ImageLoaderHelper() {
+
+    }
+}
